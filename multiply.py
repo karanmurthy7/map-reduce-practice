f85a874268dfa84033b465cbb3d19af3d806b869
Karan Murthy <karanmurthy7.applications@gmail.com>
2017-11-02 23:43:29 -0700

    Updated file multiply.py

--- multiply.py
+++ multiply.py
@@ -7,21 +7,41 @@ Map reduce algorithm to compute the matrix multiplication A x B
 
 mr = MapReduce.MapReduce()
 
+matrix_size = 5
+
 def mapper(record):
-    # key: person_name
-    # value: friend_name
-    person = record[0]
-    friend = record[1]
-    mr.emit_intermediate(person, 1)
+    matrix, row, column, value = record
+    if matrix == "a":
+        for n in range(matrix_size):
+            mr.emit_intermediate((row, n), record)
+    else:
+        # In case matrix is b
+        for n in range(matrix_size):
+            mr.emit_intermediate((n, column), record)
 
 def reducer(key, list_of_values):
-    # key: person_name
-    # value: list of 1's for every friend that
-    # the person has
-    total_friends = 0
+    # key: tuple containing (rowId, columnId)
+    # value: records from both matrix A and B as per the 
+    # condition in the mapper
+    matrix_A = []
+    matrix_B = []
     for v in list_of_values:
-      total_friends += v
-    mr.emit((key, total_friends))
+        if "a" in v:
+            matrix_A.append(v)
+        else:
+            matrix_B.append(v)
+            
+    output = 0
+    row_index = 2
+    column_index = 1
+    value_index = 3
+    
+    for a in matrix_A:
+        for b in matrix_B:
+            if a[row_index] == b[column_index]:
+                output += a[value_index] * b[value_index]
+                    
+    mr.emit((key[0], key[1], output))
 
 if __name__ == '__main__':
   inputdata = open(sys.argv[1])
